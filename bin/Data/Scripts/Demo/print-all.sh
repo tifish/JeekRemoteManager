5abeab1417c27d34557ac7513c62acc8a0ad09c3
Tinyfish <tifish@gmail.com>
2026-07-03 21:10:09 +0800

    Remove demo wait and rename failing demo script to indicate failure

--- bin/Data/Scripts/Demo/print-all.sh
+++ bin/Data/Scripts/Demo/print-all.sh
@@ -6,8 +6,5 @@ printf 'COUNT=%s\n' "$COUNT"
 printf 'ENABLED=%s\n' "$ENABLED"
 printf 'MODE=%s\n' "$MODE"
 
-printf 'waiting for 3 seconds...\n'
-sleep 3
-
 # Demo only: printing a secret exposes it in the execution log.
 printf 'TOKEN=%s\n' "$TOKEN"
--- bin/Data/Scripts/Demo/print-summary-fail.sh
+++ bin/Data/Scripts/Demo/print-summary-fail.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-printf 'Demo / print-summary\n'
+printf 'Demo / print-summary-fail\n'
 printf 'mode=%s enabled=%s count=%s\n' "$MODE" "$ENABLED" "$COUNT"
 printf 'text length=%s\n' "$(printf '%s' "$TEXT" | wc -c | tr -d ' ')"
 printf 'token length=%s\n' "$(printf '%s' "$TOKEN" | wc -c | tr -d ' ')"
